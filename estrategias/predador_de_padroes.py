5d0788ba742b69a5747f6464d06f9560db4315b2
Rojaao <roggerpk@gmail.com>
2025-06-23 14:07:45 -0300

    Delete estrategias/predador_de_padroes.py

--- estrategias/predador_de_padroes.py
+++
@@ -1,5 +0,0 @@
-def executar_estrategia(ticks):
-    # Exemplo: entra se os dois últimos dígitos forem maiores que 5
-    if len(ticks) >= 2 and int(ticks[-1][-1]) > 5 and int(ticks[-2][-1]) > 5:
-        return "CALL"
-    return None
